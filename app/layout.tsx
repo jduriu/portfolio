a4c8113fd379b2b26058dc8522d13d1667e3585e
jduriu <jduriu@gmail.com>
2023-10-30 15:51:47 -0700

    refactor landing page for grid layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,6 @@ export default function RootLayout({
         </div>
         </div>
         {children}
-
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -12,27 +12,25 @@ export default function HomePage() {
   ]
 
   return (
-    <div
-    className="bg-forestbw bg-center bg-cover bg-scroll
-    flex-col sm:flex items-center justify-center flex-wrap
-    h-full w-full overflow-scroll"
-    >
-      <div className="flex sm:w-1/2 flex-col justify-center items-center px-10 py-10 mt-[50px]">
+  <div className="w-full h-full">
+    <div className="bg-forestbw bg-center bg-cover h-full w-full"/>
+    <div className="h-[calc(100%-50px)] w-full md:grid grid-cols-2 fixed top-0 overflow-y-scroll">
+      <div className="flex flex-col justify-center items-center px-10 py-10 mt-[50px]">
           <div className="md:text-7xl text-6xl">Jon Uriu</div>
           <div className="py-10">
             <Fade inputs={tagInputs} delay={3000}/>
           </div>
       </div>
-      <div className="flex sm:w-1/2 flex-col justify-center items-center px-10 py-10 mt-[50px]">
+      <div className="flex flex-col justify-center items-center px-10 md:py-10 md:mt-[50px] ">
         <Image
           src={SelfPortrait}
           width={500}
           height={700}
           alt="Jon Uriu Image"
-          className="rounded-xl"
-          sizes="(min-width: 250px)"
+          className="rounded-xl w-[75%]"
         />
       </div>
     </div>
-  )
+  </div>
+)
 }
