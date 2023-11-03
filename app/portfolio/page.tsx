2e15edcd01c81b1bb3893b4a69a41adada9d778c
jduriu <jduriu@gmail.com>
2023-11-03 12:03:46 -0700

    resolve mobile responsive features, [WIP] tablet responsiveness

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -23,16 +23,19 @@ export default function PortfolioPage() {
   return (
     <div className="h-full w-full">
       <div className="bg-pebblebeach bg-center bg-cover fixed top-0 w-full h-full" />
-      <div className="mt-[50px] fixed top-0 w-full h-[calc(100%-50px)] p-5 grid sm:grid-cols-4 grid-rows-4 sm:grid-rows-none overflow-y-scroll">
+      <div className="mt-[50px] fixed top-0 w-full h-[calc(100%-50px)] p-5
+        grid sm:grid-cols-4 grid-rows-4 sm:grid-rows-none overflow-y-scroll"
+      >
         <div
-          className={`${projectIsSelected ? "sm:col-span-3 sm:row-span-3 h-full" : "sm:col-span-2 row-span-1"}
-          sm:overflow-hidden sm:p-10
-          sm:h-full flex flex-wrap justify-center sm:items-center`}
+          className={`h-full flex flex-wrap justify-center sm:p-10 sm:items-center
+            ${projectIsSelected ? "row-span-3 sm:col-span-3 sm:overflow-hidden"
+            : "row-span-1 sm:row-span-3 sm:col-span-2"}
+          `}
         >
           <GlassPane
-            className={`${projectIsSelected ? "items-start justify-center h-full" : "items-center justify-center h-full"}
-            flex flex-col p-5
-            w-full rounded-lg backdrop-blur-sm sm:p-10 gap-5 sm:gap-10`}
+            className={`justify-center h-full flex flex-col p-5 w-full rounded-lg backdrop-blur-sm sm:p-10 gap-5 sm:gap-10
+              ${projectIsSelected ? "items-start" : "items-center"}
+            `}
             paneColor="bg-slate-500/50"
           >
               {projectIsSelected ? (
@@ -57,14 +60,20 @@ export default function PortfolioPage() {
           </GlassPane>
         </div>
         <div
-          className={`${projectIsSelected ? "sm:col-span-1" : "sm:col-span-2 row-span-3"}
-          overflow-hidden h-full w-full`}
+          className={`overflow-hidden h-full w-full
+            ${projectIsSelected ? "sm:col-span-1"
+            : "sm:col-span-2 row-span-3"}
+          `}
           >
-          <div className={`${projectIsSelected ? "w-full" : "h-full flex-col overflow-y-scroll"}
-          flex sm:p-10 p-5 gap-10 sm:justify-center sm:items-center
-          sm:flex-wrap sm:h-full sm:overflow-y-scroll overflow-x-scroll`}>
+          <div
+            className={`w-full flex p-5 gap-10 sm:p-10 sm:items-center
+              ${projectIsSelected ?
+              "sm:flex-wrap sm:h-full sm:overflow-y-scroll overflow-x-scroll"
+              :
+              "flex-col overflow-y-scroll h-full justify-start"}
+          `}>
             {portfolioData.map((project) => (
-              <GlassPane key={project.id} className="p-3 rounded-xl flex items-center gap-5 justify-between">
+              <GlassPane key={project.id} className="p-3 rounded-xl flex flex-col items-center justify-between gap-2">
                 <ProjectCard
                   project={project}
                   setSelectedId={setSelectedProjectIdx}
