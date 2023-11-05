d3f7226bde16fe9bdfc8d3b5dff0621d1077be93
jduriu <jduriu@gmail.com>
2023-11-05 11:53:04 -0800

    create initial contact form and setup emailjs

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,39 @@
+'use client'
+import ContactForm from '@/components/ContactForm'
+import GlassPane from '@/components/Glass'
+import { useState } from 'react'
+
 export default function ContactPage() {
+  const [sent, setSent] = useState(false)
+
     return (
       <div className="h-full w-full">
         <div className="bg-sd bg-center bg-cover fixed top-0 w-full h-full" />
+        <div className="fixed top-0 mt-[50px] w-full h-[calc(100%-50px)] p-10 flex items-center justify-center">
+            <GlassPane className="md:w-1/2 h-full rounded-3xl flex flex-col gap-10 justify-center p-10 backdrop-blur-xs" paneColor="bg-black/50">
+              <div className="justify-self-start flex flex-col gap-5 p-5">
+                <div className="text-5xl">Contact Me</div>
+                <div>Reach out to me via social media</div>
+              </div>
+              <div className="w-full flex flex-col justify-center items-center p-5">
+                {sent ?
+                  <div className="flex flex-col gap-5">
+                    <div>
+                      Thanks so much! I will respond as soon as I can
+                    </div>
+                    <button className="underline hover:scale-105 transition duration-300" onClick={() => setSent(false)}>
+                      Send Another Message
+                    </button>
+                  </div>
+                  :
+                  <div className="w-full">
+                    <div className="self-start py-5">Or fill out the form below to send me and email</div>
+                    <ContactForm sent={sent} setSent={setSent}/>
+                  </div>
+                }
+              </div>
+            </GlassPane>
+        </div>
       </div>
     )
 
---
+++ components/ContactForm.tsx
@@ -0,0 +1,77 @@
+'use client'
+import { useState } from 'react'
+import emailjs from '@emailjs/browser'
+
+export default function ContactForm({sent, setSent}) {
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
+  const [sending, setSending] = useState(false)
+  const [error, setError] = useState(false)
+  const [errorMessage, setErrorMessage] = useState("")
+
+  const errorMessages = {
+    name: "Please fill in a name"
+  }
+
+  const sendEmail = async (e) => {
+    e.preventDefault()
+    const serviceId = process.env.NEXT_PUBLIC_EM_SERVICE
+    const templateId = process.env.NEXT_PUBLIC_EM_TEMPLATE
+    const publicKey = process.env.NEXT_PUBLIC_EM_KEY
+    try {
+      setError(false)
+      setSending(true)
+      if (name && email && message) {
+        await emailjs.send(serviceId, templateId,
+          {from_email: email, from_name: name, message: message},
+          publicKey
+          )
+        setSending(false)
+        setEmail("")
+        setName("")
+        setMessage("")
+        setSent(true)
+      } else {
+        throw new Error()
+      }
+    } catch (error) {
+      setError(true)
+      setSending(false)
+    }
+
+  }
+
+  return (
+    <form className="w-full flex flex-col gap-5">
+      <label>Name*</label>
+      <input className="rounded text-black px-2" type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <label>Email*</label>
+      <input className="rounded text-black px-2" type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <label>Message*</label>
+      <textarea className="rounded text-black px-2"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <div>* indicate required fields</div>
+      <button className="px-3 py-2 border border-white/50 rounded-xl" onClick={sendEmail}>
+        {sending ?
+          "Loading" :
+          "Send"
+        }
+      </button>
+      {error &&
+      <div className="text-red-700">
+        {errorMessage}
+      </div>
+      }
+  </form>
+  )
+}
